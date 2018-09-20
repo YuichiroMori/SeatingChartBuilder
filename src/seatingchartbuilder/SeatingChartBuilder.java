43fa18c5ba83fcf93cc2866f69c952b0d29f1477
YuichiroMori <43279679+YuichiroMori@users.noreply.github.com>
2018-09-19 22:37:43 -0500

    Update SeatingChartBuilder.java

--- src/seatingchartbuilder/SeatingChartBuilder.java
+++ src/seatingchartbuilder/SeatingChartBuilder.java
@@ -6,128 +6,391 @@ import java.io.*;
 public class SeatingChartBuilder {
 
     public static void main(String[] args) throws Exception{
-        
-        Scanner input=new Scanner(new File("input.txt"));
-        int checker=0;
-        int orderIndex=0;
-        ArrayList<Integer> orderList=new ArrayList<>();
-        ArrayList<String> studentList=new ArrayList<>();
-        Map<Integer,String> seat=new TreeMap<>();
-        
-        while(input.hasNext()){
-            
-            String next=input.nextLine();
-            
-            if(next.contains("Teacher")){
-                
-                String teacher=next.replace("Teacher:", "").trim();
-                String divider="";
-                
-                for(int x=0;x<teacher.length();x++){
-                    
-                    divider+="=";
-                }
-                
-                System.out.println(teacher);
-                System.out.println(divider);
-            }
-            
-            
-            else if(next.contains("Order:")){
-                
-                String order=next.replace("Order:","").trim();
-                String[] orders=order.split(", ");
-                String output="";
-                
-                for(int x=0;x<orders.length;x++){
-                    
-                    output+=orders[x]+" ";
-                }
-                
-                BestSeat best=new BestSeat(output);
-                
-                while(best.hasNext()){
-                    
-                    orderList.add(best.next());
-                    
-                }
-            }
-            
-            else if(next.contains("Class:")&&checker==0){
-                
-                String period=next.replace("Class:","").trim();
-                System.out.println(period);
-                
-            }
-            
-            
-            else if(next.contains("Class:")&&checker==1){
-                
-                Collections.shuffle(studentList);
-                orderIndex=0;
-                
-                for(int x=0;x<studentList.size();x++){
-                    seat.put(orderList.get(orderIndex),studentList.get(x));
-                    orderIndex++;
-                }
-                
-                for(int x=1;x<orderList.size()+1;x++){
-                    
-                    if(x<10){
-                    String seatNumber="0"+Integer.toString(x)+" ";
-                    String name=seatNumber+seat.get(x);
-                    System.out.println(name);
+
+            Scanner input=new Scanner(new File("input.txt"));
+            ArrayList<Integer> orderList=new ArrayList<>();
+            ArrayList<String> studentList=new ArrayList<>();
+            ArrayList<String> extraStudents=new ArrayList<>();
+            int checker=0;
+
+
+            while(input.hasNextLine()) {
+
+                String what = input.nextLine();
+
+
+                //Checks to see if the line contains Teacher
+                if (what.contains("Teacher:")) {
+
+                    String teacher = what.replace("Teacher:", "").trim();
+                    String divider = "";
+
+                    for (int x = 0; x < teacher.length(); x++) {
+                        divider += "=";
                     }
-                    else{
-                        String seatNumber=Integer.toString(x)+" ";
-                        String name=seat.get(x);
-                        System.out.println(seatNumber+name);
+
+                    System.out.println(teacher);
+                    System.out.println(divider);
+                }
+
+
+                //Checks to see if the line contains Order
+                else if (what.contains("Order:")) {
+
+                    String order = what.replace("Order:", "").trim();
+                    order = order.replace(",", "");
+
+                    BestSeat best = new BestSeat(order);
+
+                    while (best.hasNext()) {
+                        orderList.add(best.next());
                     }
                 }
-                
-                seat.clear();
-                studentList.removeAll(studentList);
-                
-                String period=next.replace("Class:","").trim();
-                System.out.println(period);
-                
+
+                //Checks to see if the line is the first class period to be pritned
+                else if (what.contains("Class:")&&checker==0) {
+                    String period = what.replace("Class: ", "");
+                    System.out.println(period+"\n");
+                }
+
+
+                //Checks to see if the occurence is class: is more than once then prints out student names, and the next class
+                else if(what.contains("Class:")&&checker==1){
+
+                    Map<Integer,String> studentMap=new TreeMap<>();
+
+                    Collections.shuffle(studentList);
+
+                    for(int x=0;x<studentList.size();x++){
+                        studentMap.put(orderList.get(x),studentList.get(x));
+                    }
+
+                    for(int y=1;y<orderList.size()/2+1;y++){
+
+                        int half=orderList.size()/2;
+
+                        if(studentMap.containsKey(y)&&studentMap.containsKey(y+half)) {
+
+                            String student = studentMap.get(y);
+                            String studentTwo=studentMap.get(y+half);
+                            String studentSeatNumber="";
+                            String studentTwoSeatNumber="";
+
+                            if (y < 10) {
+
+                                studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                            } else {
+
+                                studentSeatNumber = Integer.toString(y) + " ";
+
+                            }
+                            if(y+half<10){
+
+                                studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                            }
+
+                            else{
+
+                                studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                            }
+
+                            studentSeatNumber=studentSeatNumber+student;
+                            studentTwoSeatNumber=studentTwoSeatNumber+studentTwo;
+
+                            for(int x=studentSeatNumber.length();x<40;x++){
+                                studentSeatNumber+=" ";
+                            }
+
+                            System.out.println(studentSeatNumber+studentTwoSeatNumber);
+                        }
+                        else if(studentMap.containsKey(y)&&!studentMap.containsKey(y+half)){
+
+                            String student=studentMap.get(y);
+                            String studentSeatNumber="";
+                            String studentTwoSeatNumber="";
+
+                            if (y < 10) {
+
+                                studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                            } else {
+
+                                studentSeatNumber = Integer.toString(y) + " ";
+
+                            }
+                            if(y+half<10){
+
+                                studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                            }
+
+                            else{
+
+                                studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                            }
+
+                            studentSeatNumber=studentSeatNumber+student;
+
+                            for(int x=studentSeatNumber.length();x<40;x++){
+                                studentSeatNumber+=" ";
+                            }
+                            System.out.println(studentSeatNumber+studentTwoSeatNumber);
+                        }
+
+                        else if(!studentMap.containsKey(y)&&studentMap.containsKey(y+half)){
+
+                            String studentSeatNumber="";
+                            String studentTwo=studentMap.get(y+half);
+                            String studentTwoSeatNumber="";
+
+                            if (y < 10) {
+
+                                studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                            } else {
+
+                                studentSeatNumber = Integer.toString(y) + " ";
+
+                            }
+                            if(y+half<10){
+
+                                studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                            }
+
+                            else{
+
+                                studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                            }
+
+                            studentTwoSeatNumber=studentTwoSeatNumber+studentTwo;
+
+                            for(int x=studentSeatNumber.length();x<40;x++){
+
+                                studentSeatNumber+=" ";
+                            }
+
+                            System.out.println(studentSeatNumber+studentTwoSeatNumber);
+
+                        }
+                        else{
+
+                            String studentSeatNumber="";
+                            String studentTwoSeatNumber="";
+
+                            if (y < 10) {
+
+                                studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                            } else {
+
+                                studentSeatNumber = Integer.toString(y) + " ";
+
+                            }
+                            if(y+half<10){
+
+                                studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                            }
+
+                            else{
+
+                                studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                            }
+
+
+                            for(int x=studentSeatNumber.length();x<40;x++){
+                                studentSeatNumber+=" ";
+                            }
+                            System.out.println(studentSeatNumber+studentTwoSeatNumber);
+
+                        }
+                    }
+
+                    if(extraStudents.size()>0){
+
+                        System.out.println("Additional Students");
+
+                        for(int x=0;x<extraStudents.size();x++){
+
+                            String extra=extraStudents.get(x);
+                            System.out.println(extra);
+                        }
+                    }
+
+                    String period = what.replace("Class: ", "");
+                    System.out.println("\n"+period);
+                    studentList.clear();
+                    extraStudents.clear();
+                }
+
+                //Adds student name to list of students
+                else {
+
+                    if(orderList.size()<=studentList.size()){
+                        extraStudents.add(what);
+                    }
+                    else {
+                        studentList.add(what);
+                        checker = 1;
+                    }
+                }
             }
-            
+
+        Map<Integer,String> studentMap=new TreeMap<>();
+
+        Collections.shuffle(studentList);
+
+        for(int x=0;x<studentList.size();x++){
+            studentMap.put(orderList.get(x),studentList.get(x));
+        }
+
+        for(int y=1;y<orderList.size()/2+1;y++){
+
+            int half=orderList.size()/2;
+
+            if(studentMap.containsKey(y)&&studentMap.containsKey(y+half)) {
+
+                String student = studentMap.get(y);
+                String studentTwo=studentMap.get(y+half);
+                String studentSeatNumber="";
+                String studentTwoSeatNumber="";
+
+                if (y < 10) {
+
+                    studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                } else {
+
+                    studentSeatNumber = Integer.toString(y) + " ";
+
+                }
+                if(y+half<10){
+
+                    studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                }
+
+                else{
+
+                    studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                }
+
+                studentSeatNumber=studentSeatNumber+student;
+                studentTwoSeatNumber=studentTwoSeatNumber+studentTwo;
+
+                for(int x=studentSeatNumber.length();x<40;x++){
+                    studentSeatNumber+=" ";
+                }
+
+                System.out.println(studentSeatNumber+studentTwoSeatNumber);
+            }
+            else if(studentMap.containsKey(y)&&!studentMap.containsKey(y+half)){
+
+                String student=studentMap.get(y);
+                String studentSeatNumber="";
+                String studentTwoSeatNumber="";
+
+                if (y < 10) {
+
+                    studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                } else {
+
+                    studentSeatNumber = Integer.toString(y) + " ";
+
+                }
+                if(y+half<10){
+
+                    studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                }
+
+                else{
+
+                    studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                }
+
+                studentSeatNumber=studentSeatNumber+student;
+
+                for(int x=studentSeatNumber.length();x<40;x++){
+                    studentSeatNumber+=" ";
+                }
+                System.out.println(studentSeatNumber+studentTwoSeatNumber);
+            }
+
+            else if(!studentMap.containsKey(y)&&studentMap.containsKey(y+half)){
+
+                String studentSeatNumber="";
+                String studentTwo=studentMap.get(y+half);
+                String studentTwoSeatNumber="";
+
+                if (y < 10) {
+
+                    studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                } else {
+
+                    studentSeatNumber = Integer.toString(y) + " ";
+
+                }
+                if(y+half<10){
+
+                    studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                }
+
+                else{
+
+                    studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                }
+
+                studentTwoSeatNumber=studentTwoSeatNumber+studentTwo;
+
+                for(int x=studentSeatNumber.length();x<40;x++){
+
+                    studentSeatNumber+=" ";
+                }
+
+                System.out.println(studentSeatNumber+studentTwoSeatNumber);
+
+            }
             else{
-                
-                studentList.add(next);
-                checker=1;
-                
+
+                String studentSeatNumber="";
+                String studentTwoSeatNumber="";
+
+                if (y < 10) {
+
+                    studentSeatNumber = "0" + Integer.toString(y) + " ";
+
+                } else {
+
+                    studentSeatNumber = Integer.toString(y) + " ";
+
+                }
+                if(y+half<10){
+
+                    studentTwoSeatNumber="0"+Integer.toString(y+half)+" ";
+                }
+
+                else{
+
+                    studentTwoSeatNumber=Integer.toString(y+half)+" ";
+                }
+
+
+                for(int x=studentSeatNumber.length();x<40;x++){
+                    studentSeatNumber+=" ";
+                }
+                System.out.println(studentSeatNumber+studentTwoSeatNumber);
+
             }
         }
-        
-                    
-        for(int x=1;x<orderList.size()+1;x++){
-            
-                Collections.shuffle(studentList);
-                orderIndex=0;
-                
-                for(int y=0;y<studentList.size();y++){
-                    seat.put(orderList.get(orderIndex),studentList.get(y));
-                    orderIndex++;
-                }
-                    
-            if(seat.get(x)!=null){
-                    if(x<10){
-                    String seatNumber="0"+Integer.toString(x)+" ";
-                    String name=seatNumber+seat.get(x);
-                    System.out.println(name);
-                    }
-                    else{
-                        String seatNumber=Integer.toString(x)+" ";
-                        String name=seat.get(x);
-                        System.out.println(seatNumber+name);
-                    }
-            }
-            else{
-                //break;
-            }
-    } 
-}
-}
+        if(extraStudents.size()>0){
 
+            System.out.println("Additional Students");
+
+            for(int x=0;x<extraStudents.size();x++){
+
+                String extra=extraStudents.get(x);
+                System.out.println(extra);
+            }
+        }
+    }
+}
